6bd17333fbdd95c07c983c6b7d700b3e865cfda0
unknown <321611@student.mboutrecht.nl>
2022-02-15 21:46:11 +0100

    er is een button toegevoegd aan de login page met de functie dat word geopend modal

--- login.php
+++ login.php
@@ -5,20 +5,81 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
     <title>login</title>
 </head>
 <body>
 
-<div class="form-floating mb-3">
-  <input type="email" class="form-control" id="floatingInput" placeholder="vul hier uw mail in">
-  
-</div>
-<div class="form-floating">
-  <input type="password" class="form-control" id="vul hier uw wachtwoord in" placeholder="Password">
-  
-</div>
-<div class="button"> <a href="">login</a> </div>  
-<div class="button"> <a href="">register</a> </div>   
+<!-- Button trigger modal -->
 
+
+<!-- Modal -->
+<div class="modal fade" id="login" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Log hier in met uw account</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+    <div class="modal-body">
+      <br>
+        <div class="form-floating mb-3 sm-12">
+          <input type="email" class="form-control" id="floatingInput" placeholder="Vul hier uw email in">
+      </div>
+      <br>
+        <div class="form-floating mb-3 sm-12">
+          <input type="password" class="form-control" id="floatingPassword" placeholder="Vul hier uw wachtwoord in">
+          <br>
+          <label style="10px" for="floatingPassword">Deel uw wachtwoord niet met anderen!
+      </div>
+      </div>
+      <br>
+      <br>
+      <br>  
+      <div class="modal-footer">
+      <button type="submit" class="btn btn-success" name="submit-login">Login</button>
+        <button type="submit" class="btn btn-info" name="submit-register">Registreer</button>
+      </div>
+    </div>
+  </div>
+</div>
+
+  <div class="container">
+<div class="row">
+  <div class="col-md-12 text-right">
+    <button class="btn btn-success badge-pill" data-toggle="modal" data-target="#login" style="width: 80px;"> Login </button>
+  </div>
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+</div>
 </body>
 </html>
