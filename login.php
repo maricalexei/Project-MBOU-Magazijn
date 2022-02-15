fcd8b2124b834e85b0e6cf4d258bfbfbec82d698
unknown <321611@student.mboutrecht.nl>
2022-02-15 16:31:44 +0100

    login pagina email en wachtwoord veld toegevoegd

---
+++ login.php
@@ -0,0 +1,24 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>login</title>
+</head>
+<body>
+
+<div class="form-floating mb-3">
+  <input type="email" class="form-control" id="floatingInput" placeholder="vul hier uw mail in">
+  
+</div>
+<div class="form-floating">
+  <input type="password" class="form-control" id="vul hier uw wachtwoord in" placeholder="Password">
+  
+</div>
+<div class="button"> <a href="">login</a> </div>  
+<div class="button"> <a href="">register</a> </div>   
+
+</body>
+</html>
