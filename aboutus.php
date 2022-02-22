79132e3b58ea0c7f04a352762de9e35e1ea81267
AronVLD <327156@student.mboutrecht.nl>
2022-02-22 10:34:25 +0100

    Added Aboutus page

---
+++ aboutus.php
@@ -0,0 +1,27 @@
+<div class="section">
+    <div class="container">
+        <div class="row">
+            <div class="col-4">
+                <div class="content-section">
+                    <div class="title">
+                        <h1>About Us</h1>
+                        <hr>
+                    </div>
+                    <div class="content">
+                        <h3>This is us</h3>
+                        <p><br>We are Warehouse MBO Utrecht<br>
+                            <br> In the warehouse we try to become the best as possible to achieve greatness <br>
+                            <br> the warehouse is located in the city utrecht <br>
+                        </p>
+                        <hr>
+                    </div>
+                </div>
+            </div>
+            <div class="col4">
+                <div class="image-section">
+                    <img src="img/.jpg">
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
