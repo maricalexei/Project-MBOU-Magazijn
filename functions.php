d7c70aaef0d71ff4b27865c08f0fc3d1a234633a
unknown <321611@student.mboutrecht.nl>
2022-03-16 16:40:35 +0100

    functions php de database voor login er in gezet

--- functions.php
+++ functions.php
@@ -1,1 +1,9 @@
-git
+<?php
+  function sanitize($raw_data) {
+    global $conn;
+    $data = htmlspecialchars($raw_data);
+    $data = mysqli_real_escape_string($conn, $data);
+    $data = trim($data);
+    return $data;
+  }
+?> 
