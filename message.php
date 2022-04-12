c60ce0b54c2c13639c52dd26de617c01764a6c3b
Yusuf <321611@student.mboutrecht.nl>
2022-04-12 23:04:59 +0200

    massage alert aangepasst

--- message.php
+++ message.php
@@ -160,7 +160,7 @@ switch($alert) {
         echo '<div class="alert alert-success mt-5 w-50 mx-auto text-center" role="alert">
             De gegevens zijn succesvol opgeslagen.
             </div>';
-        header("Refresh: 3; ./index.php?content=lendoutoverview");
+        header("Refresh: 1; ./index.php?content=lendoutread");
         break;
     case "lendoutinfo-failed":
         echo '<div class="alert alert-danger mt-5 w-50 mx-auto text-center" role="alert">
