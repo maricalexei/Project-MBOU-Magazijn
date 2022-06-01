354ba028fb5ff778dbbb8b991324ec7f665ec15f
Maric-Alexei Brouwer <56292223+maricalexei@users.noreply.github.com>
2022-06-01 14:43:48 +0200

    Revert "Dasboard super user"

---
+++ aboutus.php
@@ -0,0 +1,30 @@
+<?php include_once("./navigation.php");?>
+    <div class="container">
+        <div class="row">
+            <div class="col-4">
+                <div class="content-section">
+                    <!-- title -->
+                    <div class="title">
+                        <h1>About Us</h1>
+                        <hr>
+                    </div>
+                    <!-- text -->
+                    <div class="content">
+                        <h3>This is us</h3>
+                        <p><br>We are Warehouse MBO Utrecht<br>
+                            <br> In the warehouse we try to become the best as possible to achieve greatness <br>
+                            <br> the warehouse is located in the city utrecht <br>
+                        </p>
+                        <hr>
+                    </div>
+                </div>
+            </div>
+            <!-- image -->
+            <div class="col4">
+                <div class="image-section">
+                    <img src="img/aboutus.jpg">
+                </div>
+            </div>
+        </div>
+    </div>
+    </div>
